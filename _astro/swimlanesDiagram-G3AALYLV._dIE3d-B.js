02919d0a273806a9a45c73e87f8d93b65950b1fe
Marcin Kossakowski <marcin.kossakowski@gmail.com>
2026-07-29 18:00:22 -0600

    Updates

---
+++ _astro/swimlanesDiagram-G3AALYLV._dIE3d-B.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./src.k7_XveP_.js";import"./chunk-WYO6CB5R.o2_MXgCY.js";import"./chunk-ICXQ74PX.BI2eWg4Z.js";import"./chunk-HOUHSVGY.oAFWuSeO.js";import"./chunk-Q4XR5HBZ.CyGni5jL.js";import"./chunk-7BUUIJ7U.CAhmNhj7.js";import"./chunk-OGEWGWER.BGVLOWXN.js";import"./chunk-32BRIVSS.CzRGqYgr.js";import"./chunk-XXDRQBXY.DquYCiZO.js";import"./chunk-VR4S4FIN.DnXY4gwx.js";import"./chunk-C7G6YPKG.D4C5GKZA.js";import"./chunk-ZGVPDNZ5.DxHpDC47.js";import"./chunk-52WLFC77.D2ZWMFef.js";import"./chunk-FWX5IMBZ.BfbC82Xi.js";import"./chunk-ZIRB5QZD.Dh20_f-h.js";import{r as t,t as n}from"./chunk-PUDLZKDR.BsRg1IhW.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
